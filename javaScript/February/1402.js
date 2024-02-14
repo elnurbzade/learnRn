a2c205cd139ae38190f690b1b5fefa0d97bea89d
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-02-14 15:04:48 +0400

    1402

--- javaScript/February/1402.js
+++ javaScript/February/1402.js
@@ -83,69 +83,100 @@ const order103 = orders.findIndex((order) => order.orderId === 103);
 console.log("Index of 103:", order103);
 
 // Check for Orders in a Specific Country
-const ordersInFrance = orders.some((order)=>customers.find((customer)=> customer.id===order.customerId).location.country ==='France')
-console.log('Orders in France:', ordersInFrance);
+const ordersInFrance = orders.some(
+  (order) =>
+    customers.find((customer) => customer.id === order.customerId).location
+      .country === "France"
+);
+console.log("Orders in France:", ordersInFrance);
 
+orders.forEach((order) => {
+  const customerName = customers.find(
+    (customer) => customer.id === order.customerId
+  ).name;
+  console.log(
+    `Order ${order.orderId} by ${customerName}: ${order.quantity} x ${order.product} for $${order.price} each.`
+  );
+});
 
-// Print Order Summaries
-orders.forEach(order => {
-    const customerName = customers.find(customer => customer.id === order.customerId).name;
-    console.log(`Order ${order.orderId} by ${customerName}: ${order.quantity} x ${order.product} for $${order.price} each.`);
-  });
-
-// Sort Customers by Name
-const sortedCustomers = customers.slice().sort((a, b) => a.name.localeCompare(b.name));
-console.log('Sorted Customers by Name:', sortedCustomers);
+const sortedCustomers = customers
+  .slice()
+  .sort((a, b) => a.name.localeCompare(b.name));
+console.log("Sorted Customers by Name:", sortedCustomers);
 
 console.log("---------------------------------------------------------");
-// // Adding Tasks
-// const order102Index = orders.findIndex((order) => order.orderId === 102);
-// console.log("Index of Order 102:", order102Index);
+// Adding Tasks
+const order102Index = orders.findIndex((order) => order.orderId === 102);
+console.log("Index of Order 102:", order102Index);
 
-// const ordersInUSA = orders.some(
-//   (order) =>
-//     customers.find((customer) => customer.id === order.customerId).location
-//       .country === "USA"
-// );
-// console.log("Orders in USA:", ordersInUSA);
+const ordersInUSA = orders.some(
+  (order) =>
+    customers.find((customer) => customer.id === order.customerId).location
+      .country === "USA"
+);
+console.log("Orders in USA:", ordersInUSA);
 
-// // Map //
+// Map //
 
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-// function double(arr) {
-//   return arr.map((num) => num * 2);
-// }
-// console.log(double(numbers));
+function double(arr) {
+  return arr.map((num) => num * 2);
+}
+console.log(double(numbers));
 
-// function trio(arr) {
-//   return arr.map((num) => num * 3);
-// }
-// console.log(trio(numbers));
+function trio(arr) {
+  return arr.map((num) => num * 3);
+}
+console.log(trio(numbers));
 
-// function four(arr) {
-//   return arr.map((num) => num * 4);
-// }
+function four(arr) {
+  return arr.map((num) => num * 4);
+}
 
-// console.log(four(numbers));
+console.log(four(numbers));
 
-// function bole(arr) {
-//   return arr.map((num) => num / 2);
-// }
+function bole(arr) {
+  return arr.map((num) => num / 2);
+}
 
-// console.log(bole(numbers));
+console.log(bole(numbers));
 
-// function mix(arr) {
-//   return arr.map((num) => num * 8 + 122 - 221);
-// }
+function mix(arr) {
+  return arr.map((num) => num * 8 + 122 - 221);
+}
 
-// console.log(mix(numbers));
+console.log(mix(numbers));
 
-// function redNum(arr) {
-//   return arr.reduce((test1, test2)=> test1+test2);
-// }
+function redNum(arr) {
+  return arr.reduce((test1, test2) => test1 + test2);
+}
 
-// console.log(redNum(numbers));
+console.log(redNum(numbers));
+
+const items = [
+  { name: "Apple", price: 1, tax: 10 },
+  { name: "Orange", price: 2, tax: 15 },
+  { name: "Mango", price: 3, tax: 20 },
+];
+
+let mixVal = 5;
+// items.forEach(item => mixVal+=item.tax+=item.price)
+items.forEach((item) => (item.price += mixVal));
+console.log(items);
+
+let mixVal2 = [items.price, items.tax];
+console.log(mixVal2);
+
+let totalPrice = 0;
+items.forEach((item) => (totalPrice += item.price));
+console.log(totalPrice);
+
+totalTax = 0;
+items.forEach((item) => {
+  totalTax += item.tax;
+});
+console.log(totalTax);
 
 // const items = [
 //     { name: "Apple", price: 1, tax: 10 },
@@ -153,114 +184,90 @@ console.log("---------------------------------------------------------");
 //     { name: "Mango", price: 3, tax: 20 },
 //   ];
 
-//   let mixVal = 5;
-//   // items.forEach(item => mixVal+=item.tax+=item.price)
-//   items.forEach((item) => (item.price += mixVal));
-//   console.log(items);
+const withRed = items.reduce((acc, val) => {
+  return (acc += val.price);
+}, 0);
 
-//   let mixVal2 = [items.price, items.tax];
-//   console.log(mixVal2);
+console.log(withRed);
 
-//   let totalPrice = 0;
-//   items.forEach((item) => (totalPrice += item.price));
-//   console.log(totalPrice);
+const withRed2 = items.reduce((acc, val) => {
+  return (acc += val.tax);
+}, 0);
+console.log(withRed2);
 
-//   totalTax = 0;
-//   items.forEach((item) => {
-//     totalTax += item.tax;
-//   });
-//   console.log(totalTax);
+const withRed3 = items.reduce((acc, val, val2) => {
+  return (acc += val.price += val.tax);
+}, 0);
+console.log(withRed3);
 
-//   // const items = [
-//   //     { name: "Apple", price: 1, tax: 10 },
-//   //     { name: "Orange", price: 2, tax: 15 },
-//   //     { name: "Mango", price: 3, tax: 20 },
-//   //   ];
+const items3 = [
+  { name: "Apple", category: "Fruit" },
+  { name: "Onion", category: "Vegetable" },
+  { name: "Orange", category: "Fruit" },
+  { name: "Lettuce", category: "Vegetable" },
+];
 
-//   const withRed = items.reduce((acc, val) => {
-//     return (acc += val.price);
-//   }, 0);
+const groupedItems = items3.reduce((accumulator, item) => {
+  const category = item.category;
+  if (!accumulator[category]) {
+    accumulator[category] = [];
+  }
+  accumulator[category].push(item.name);
+  return accumulator;
+}, {});
 
-//   console.log(withRed);
+console.log(groupedItems);
 
-//   const withRed2 = items.reduce((acc, val) => {
-//     return (acc += val.tax);
-//   }, 0);
-//   console.log(withRed2);
+const items2 = [
+  { name: "Elnur", surName: "Bayramzada", id: 525 },
+  { name: "Elshen", surName: "Huseynov", id: 1025 },
+  { name: "Omer", surName: "Bayramzada", id: 525 },
+  { name: "Fuad", surName: "Huseynov", id: 1025 },
+  { name: "Ali", surName: "Bayramzada", id: 525 },
+];
 
-//   const withRed3 = items.reduce((acc, val, val2) => {
-//     return (acc += val.price += val.tax);
-//   }, 0);
-//   console.log(withRed3);
+const groupItems2 = items2.reduce((acc, val) => {
+  const surName = val.surName;
+  if (!acc[surName]) {
+    acc[surName] = [];
+  }
+  acc[surName].push(val.name) && console.log("id:", acc[surName].push(val.id));
+  return acc;
+}, {});
 
-//   const items3 = [
-//     { name: "Apple", category: "Fruit" },
-//     { name: "Onion", category: "Vegetable" },
-//     { name: "Orange", category: "Fruit" },
-//     { name: "Lettuce", category: "Vegetable" },
-//   ];
+console.log(groupItems2);
 
-//   const groupedItems = items3.reduce((accumulator, item) => {
-//     const category = item.category;
-//     if (!accumulator[category]) {
-//       accumulator[category] = [];
-//     }
-//     accumulator[category].push(item.name);
-//     return accumulator;
-//   }, {});
+function greeting() {
+  console.log("Hello World");
+  5000;
+}
 
-//   console.log(groupedItems);
+setTimeout(greeting, 5000);
 
-//   const items2 = [
-//     { name: "Elnur", surName: "Bayramzada", id: 525 },
-//     { name: "Elshen", surName: "Huseynov", id: 1025 },
-//     { name: "Omer", surName: "Bayramzada", id: 525 },
-//     { name: "Fuad", surName: "Huseynov", id: 1025 },
-//     { name: "Ali", surName: "Bayramzada", id: 525 },
-//   ];
+clearTimeout(greeting);
 
-//   const groupItems2 = items2.reduce((acc, val) => {
-//     const surName = val.surName;
-//     if (!acc[surName]) {
-//       acc[surName] = [];
-//     }
-//     acc[surName].push(val.name) && console.log("id:", acc[surName].push(val.id));
-//     return acc;
-//   }, {});
+function testSet(name, role) {
+  console.log(`Hello my name is ${name}`);
+  console.log(`I am ${role}`);
+}
+// setTimeout(testSet, 5000, "Elnur", "Software Developer");
+setTimeout(() => {
+  testSet("Elnur", "Software Developer");
+}, 2000);
+clearTimeout(testSet);
+console.log(`${testSet} is cleared`);
 
-//   console.log(groupItems2);
+const timeOutId = setTimeout(function () {
+  console.log("Hello World");
+}, 2000);
 
-//   function greeting() {
-//     console.log("Hello World");
-//     5000;
-//   }
+clearTimeout(timeOutId);
+console.log(`Timeout ID ${timeOutId} has been cleaned`);
 
-//   setTimeout(greeting, 5000);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const found = arr.find((num) => num > 4);
+console.log(found);
 
-//   clearTimeout(greeting);
-
-//   function testSet(name, role) {
-//     console.log(`Hello my name is ${name}`);
-//     console.log(`I am ${role}`);
-//   }
-//   // setTimeout(testSet, 5000, "Elnur", "Software Developer");
-//   setTimeout(() => {
-//     testSet("Elnur", "Software Developer");
-//   }, 2000);
-//   clearTimeout(testSet);
-//   console.log(`${testSet} is cleared`);
-
-//   const timeOutId = setTimeout(function () {
-//     console.log("Hello World");
-//   }, 2000);
-
-//   clearTimeout(timeOutId);
-//   console.log(`Timeout ID ${timeOutId} has been cleaned`);
-
-//   const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-//   const found = arr.find((num) => num > 4);
-//   console.log(found);
-
-//   const arr2 = [12, 32, 436, 7567, 3543, 334, 455, 22, 256, 353, 5];
-//   const found2 = arr2.find((num) => num % 13 === 0);
-//   console.log(found2);
+const arr2 = [12, 32, 436, 7567, 3543, 334, 455, 22, 256, 353, 5];
+const found2 = arr2.find((num) => num % 13 === 0);
+console.log(found2);

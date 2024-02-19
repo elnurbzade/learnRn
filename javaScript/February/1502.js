b65ccc7960df498f79e6cdefa826a8441622d518
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-02-19 16:35:35 +0400

    19022024

---
+++ javaScript/February/1502.js
@@ -0,0 +1,13 @@
+const inventory = [
+    { name: "asparagus", type: "vegetables", quantity: 5 },
+    { name: "bananas", type: "fruit", quantity: 0 },
+    { name: "goat", type: "meat", quantity: 23 },
+    { name: "cherries", type: "fruit", quantity: 5 },
+    { name: "fish", type: "meat", quantity: 22 },
+  ];
+
+  function myCallback({ quantity }) {
+    return quantity > 5 ? "ok" : "restock";
+  }
+  
+  const result2 = Object.groupBy(inventory, myCallback);
---
+++ javaScript/February/helloworld.ts
@@ -0,0 +1,12 @@
+function say() {
+  console.log("Hello World");
+}
+say();
+
+var maash = 1000;
+
+var realMaash = (maash * 1000) / 30;
+
+console.log(realMaash);
+
+

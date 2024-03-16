57c10107d1298784cd8fbf1e07617fa0cc0234bc
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-03-17 03:26:43 +0400

    Hello TS

--- TypeScript/task.js
+++ TypeScript/task.js
@@ -8,3 +8,16 @@ console.log(num);
 console.log(e);
 let message = "Hello New Stack";
 console.log(message);
+let obj = { x: 0 };
+console.log(obj);
+let myName2 = "Alice";
+console.log(myName2);
+function greet(name) {
+    console.log("Hello, " + name.toUpperCase() + " !!!");
+}
+greet("john");
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const obj: number = obj;
--- TypeScript/task.ts
+++ TypeScript/task.ts
@@ -9,7 +9,23 @@ const num: number = calculate(20, 25);
 console.log(num);
 console.log(e);
 
-let message: string = "Hello New Stack"
+let message: string = "Hello New Stack";
 
 console.log(message);
+
+let obj: any = { x: 0 };
+console.log(obj);
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const obj: number = obj;
+
+let myName2: string = "Alice";
+console.log(myName2);
+
+function greet(name: string) {
+  console.log("Hello, " + name.toUpperCase() + " !!!");
+}
+greet("john");
 

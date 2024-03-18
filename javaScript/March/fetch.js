64054fc5105dacc2b1366fcd1beb811f40ba17e6
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-03-18 15:25:10 +0400

    it is ok

---
+++ javaScript/March/fetch.js
@@ -0,0 +1,7 @@
+async function logMovies() {
+  const response = await fetch("http://www.boredapi.com/api/activity?key=5881028");
+  const movies = await response.json();
+  console.log(movies);
+}
+
+console.log(logMovies())
